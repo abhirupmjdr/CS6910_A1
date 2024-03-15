7fbd0c2c348d744951ae2e3684174303af0ef0e0
“Abhirup <abhirupmajumder2@gmail.com>
2024-03-16 01:32:53 +0530

    change and commented q2

--- Question2.py
+++ Question2.py
@@ -1,6 +1,4 @@
-
-#question 2
-
+# Import necessary libraries
 import numpy as np
 from keras.datasets import fashion_mnist
 from sklearn.model_selection import train_test_split
@@ -23,29 +21,30 @@ y_train_T = y_train.reshape(1, -1)
 y_val_T = y_val.reshape(1, -1)
 y_test_T = y_test.reshape(1, -1)
 
+# Define a utility class with static methods for activation functions, loss calculation, and accuracy calculation
 class Util:
-
+    # Method to apply the specified activation function
     @staticmethod
     def apply_activation(A, activation):
-            if activation == 'sigmoid':
-                return Compute.sigmoid(A)
-            elif activation == 'ReLU':
-                return Compute.Relu(A)
-            elif activation == 'tanh':
-                return Compute.tanh(A)
+        if activation == 'sigmoid':
+            return Compute.sigmoid(A)
+        elif activation == 'ReLU':
+            return Compute.Relu(A)
+        elif activation == 'tanh':
+            return Compute.tanh(A)
 
+    # Method to calculate the specified loss
     @staticmethod
     def loss(input, true_output, predicted_output, loss, batch_size,n_output):
         if loss == 'cross_entropy':
             one_hot_true_output = np.eye(n_output)[true_output[0]].T
             return -np.sum(one_hot_true_output * np.log(predicted_output + 1e-9)) / batch_size
-
-
         if loss=='mean_squared_error':
             one_hot_true_output = np.eye(n_output)[true_output[0]].T
             loss_factor=np.square(predicted_output-one_hot_true_output)
             return np.sum(loss_factor)/batch_size
 
+    # Method to calculate the accuracy
     @staticmethod
     def accuracy(input, true_output, predicted_output):
         predicted_labels = np.argmax(predicted_output, axis=0)
@@ -54,75 +53,88 @@ class Util:
         accuracy_percentage = (correct_predictions / total_samples) * 100
         return accuracy_percentage
 
-
+# Define a class with static methods for various computations
 class Compute:
-
+    # Method to calculate the sigmoid of x
     @staticmethod
     def sigmoid(x):
         return  1 / (1 + np.exp(-x))
 
+    # Method to calculate the softmax of x
     @staticmethod
     def softmax(x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
+    # Method to calculate the ReLU of x
     @staticmethod
     def Relu(x):
         return np.maximum(x,0)
 
+    # Method to calculate the tanh of x
     @staticmethod
     def tanh(x):
         return (2 * Compute.sigmoid(2 * x)) - 1
 
+    # Method to calculate the derivative of the softmax function
     @staticmethod
     def softmax_derivative(x):
         return Compute.softmax(x) * (1-Compute.softmax(x))
 
+    # Method to calculate the derivative of the sigmoid function
     @staticmethod
     def sigmoid_derivative(Z):
         s = Compute.sigmoid(Z)
         dA = s * (1 - s)
         return dA
 
+    # Method to calculate the derivative of the ReLU function
     @staticmethod
     def Relu_derivative(x):
         return 1*(x > 0)
 
+    # Method to calculate the derivative of the tanh function
     @staticmethod
     def tanh_derivative(x):
         return (1 - (Compute.tanh(x)**2))
 
+    # Method to calculate the gradients
     @staticmethod
     def calculate_gradients(k, dA, H_prev, A_prev, W, activation, batch_size):
-            dW = Compute.calculate_dW(dA, H_prev, batch_size)
-            db = Compute.calculate_db(dA, batch_size)
-            dH_prev, dA_prev = Compute.calculate_dH_prev_dA_prev(k, W, dA, activation, A_prev) if k > 1 else (np.zeros(H_prev.shape), np.zeros(A_prev.shape))
+        dW = Compute.calculate_dW(dA, H_prev, batch_size)
+        db = Compute.calculate_db(dA, batch_size)
+        dH_prev, dA_prev = Compute.calculate_dH_prev_dA_prev(k, W, dA, activation, A_prev) if k > 1 else (np.zeros(H_prev.shape), np.zeros(A_prev.shape))
+        return dW, db, dH_prev, dA_prev
 
-            return dW, db, dH_prev, dA_prev
+    # Method to calculate dW
     @staticmethod
     def calculate_dW(dA, H_prev, batch_size):
-            return np.dot(dA, H_prev.T) / batch_size
+        return np.dot(dA, H_prev.T) / batch_size
 
+    # Method to calculate db
     @staticmethod
     def calculate_db(dA, batch_size):
-            return np.sum(dA, axis=1, keepdims=True) / batch_size
+        return np.sum(dA, axis=1, keepdims=True) / batch_size
 
+    # Method to calculate dH_prev and dA_prev
     @staticmethod
     def calculate_dH_prev_dA_prev(k, W, dA, activation, A_prev):
-            dH_prev = np.matmul(W.T, dA)
-            dA_prev = Compute.calculate_dA_prev(dH_prev, activation, A_prev)
-            return dH_prev, dA_prev
+        dH_prev = np.matmul(W.T, dA)
+        dA_prev = Compute.calculate_dA_prev(dH_prev, activation, A_prev)
+        return dH_prev, dA_prev
 
+    # Method to calculate dA_prev
     @staticmethod
     def calculate_dA_prev(dH_prev, activation, A_prev):
-            if activation == 'sigmoid':
-                return dH_prev * Compute.sigmoid_derivative(A_prev)
-            elif activation == 'tanh':
-                return dH_prev * Compute.tanh_derivative(A_prev)
-            elif activation == 'ReLU':
-                return dH_prev * Compute.Relu_derivative(A_prev)
+        if activation == 'sigmoid':
+            return dH_prev * Compute.sigmoid_derivative(A_prev)
+        elif activation == 'tanh':
+            return dH_prev * Compute.tanh_derivative(A_prev)
+        elif activation == 'ReLU':
+            return dH_prev * Compute.Relu_derivative(A_prev)
 
-
+# Define a class for the neural network
 class NeuralNetwork:
+    # Initialize the neural network
     def __init__(self, init_mode='random', num_hidden_layers=3, neurons_per_hidden_layer=64,
                  activation_function='sigmoid', train_input=x_train_T, train_output=y_train_T, val_input=x_val_T,
                  val_output=y_val_T):
@@ -143,6 +155,7 @@ class NeuralNetwork:
         self.grads = {}
         self.initialize_parameters()
 
+    # Method to initialize the parameters
     def initialize_parameters(self):
         for l in range(1, self.n_layers):
             if self.init_mode == "random":
@@ -153,15 +166,8 @@ class NeuralNetwork:
                 self.theta["W" + str(l)] = np.random.normal(0.0, limit, size=(self.n_neurons[l],self.n_neurons[l - 1]))
             self.theta["b" + str(l)] = np.zeros((self.n_neurons[l] , 1))
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
 
-    def relu(self, x):
-        return np.maximum(x, 0)
-
-    def tanh(self, x):
-        return np.tanh(x)
-
+    # Method to perform a forward pass
     def forward(self, X):
         self.cache["H0"] = X
         for l in range(1, self.n_layers):
